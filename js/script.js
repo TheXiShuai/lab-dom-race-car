7c2f6b9bf5d9dd60caacadb33c7d3a0b3609e0b8
John Doe <johndoe@example.com>
2023-07-20 17:58:50 +0200

    gaushsah

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
 window.onload = function () {
+  console.log('SCRIPT JS IS WORKING');
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let game;
   startButton.addEventListener("click", function () {
+    console.log('START BUTTON IS WORKING');
     startGame();
   });
 
   function startGame() {
     console.log("start game");
+    game = new Game();
+    game.start();
   }
+  function handleKeyboardInput(key){
+    if(key === "ArrowUp"){
+      game.player.directionY = -1;
+    }
+    else if(key === "ArrowDown"){
+      game.player.directionY = 1;
+    }
+    else if(key === "ArrowLeft"){
+      game.player.directionX = -1;
+    }
+    else if(key === "ArrowRight"){
+      game.player.directionX = 1;
+    }
+  }
+  window.addEventListener("keydown",(event)=>{
+    handleKeyboardInput(event.key)
+  })
+
 };
